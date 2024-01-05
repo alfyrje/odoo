afa916f634d25456c975b55c734fa8380739a264
alfyrje <alfyrje@gmail.com>
2024-01-05 17:08:04 +0700

    added real estate menu

---
+++ test/estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'estate',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',    
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ],
+    'application' : 'true',
+}
